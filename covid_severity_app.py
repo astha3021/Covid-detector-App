d93fb2b2ff55d18be88bb9dc8091466034ccfae1
astha3021 <47379203+astha3021@users.noreply.github.com>
2021-07-09 13:01:52 +0530

    Update covid_severity_app.py

--- covid_severity_app.py
+++ covid_severity_app.py
@@ -69,13 +69,13 @@ def main():
     symptoms_list = ['Breathing Problem','Fever','Dry Cough','Sore Throat','Running Nose','Asthma','Chronic Lung Disease','Headache','Heart Disease','Diabetes','Hyper Tension','Fatigue','Gastrointestinal','Abroad travel','Contact with COVID Patient','Attended Large Gathering','Visited Public Exposed Places','Family working inpublic exposed places']
     
     if option == 'Detect Covid':
-      hide_streamlit_style = """
+        hide_streamlit_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
                 </style>
                 """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
         
         st.markdown('<p class= "title">\U0001F637 COVID-19 SEVERITY DETECTION MODEL \U0001F637 </p>',unsafe_allow_html=True)
         st.write("\n")
